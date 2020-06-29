a9a0ae6f7f239c18c4ca40304105957ca11fbab7
James Lowther <jamesevlowther@gmail.com>
2020-06-28 18:53:04 -0600

    Add villages

--- src/villages.py
+++ src/villages.py
@@ -1,7 +1,53 @@
 from discord.ext import commands
 from cfg import bot
 
+import cfg
 
-@bot.command(name="villages"):
-async def show_villages(ctx):
+
+@bot.command(name="village"):
+async def village_options(ctx, *args):
+    if len(args) == 0:
+        show_village_help(ctx)
+        return
+
+    action = args[0].lower()
+
+    if action == "list":
+
+    else if action == "buy":
+        if len(args) < 2:
+            await ctx.channel.send("Use the command `" + cfg.PREFIX + "buy <name>` to purchase a new village.")
+            return
+
+    else if action == "upgrade":
+        if len(args) < 2:
+            await ctx.channel.send("Use the command `" + cfg.PREFIX + "upgrade <index>` to upgrade a village.")
+            return
+
+    else if action == "rename":
+        if len(args) < 2:
+            await ctx.channel.send("Use the command `" + cfg.PREFIX + "rename <index> <name>` to rename a village.")
+            return
+    else:
+        show_village_help(ctx)
+
+
+async def show_village_help(ctx):
+    pass
+
+
+async def list_villages(ctx):
+    
+    cfg.db_cur.execute("SELECT * FROM Villages v, Kingdoms k WHERE ")
+
+
+async def buy_village(ctx, args):
+    pass
+
+
+async def upgrade_village(ctx, args):
+    pass
+
+
+async def rename_village(ctx, args):
     pass
